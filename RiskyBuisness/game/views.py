21aadb0253084835ba2fdfc4395a7564169b6e47
Gray486 <me@grayjn.com>
2023-08-17 09:50:22 -0400

    Fix issue with templates only loading home page

--- RiskyBuisness/game/views.py
+++ RiskyBuisness/game/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def game_show(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('game.html')
     return HttpResponse(template.render())
 
--- RiskyBuisness/home/views.py
+++ RiskyBuisness/home/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def home_show(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('home.html')
     return HttpResponse(template.render())
 
