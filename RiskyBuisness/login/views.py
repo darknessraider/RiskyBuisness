2c424b8386d9f7bf5f22c014092104956c751f89
The Darkness that is everywhere and nowhere and that includes you mother's pussy <woodyspivey@gmail.com>
2023-08-23 14:12:17 -0400

    Make Username And Password Be Stored In Session

--- RiskyBuisness/login/views.py
+++ RiskyBuisness/login/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import LoginForm
+from django.http import HttpResponseRedirect
 
 def login_show(request):
-    template = loader.get_template('login.html')
-    return HttpResponse(template.render())
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session["username"] = form.cleaned_data["username"]
+            request.session["password"] = form.cleaned_data["password"]
+            return HttpResponseRedirect('/')
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form})
