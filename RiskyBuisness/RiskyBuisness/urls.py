bbeb06228d652765a7454dea46ed41a1ee2f5204
The Darkness that is everywhere and nowhere and that includes you mother's pussy <woodyspivey@gmail.com>
2023-08-23 14:39:09 -0400

    Stop Merge Conflicts

--- RiskyBuisness/RiskyBuisness/urls.py
+++ RiskyBuisness/RiskyBuisness/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("home.urls")), 
-    path('game/', include("game.urls")), 
 ]
--- RiskyBuisness/home/views.py
+++ RiskyBuisness/home/views.py
@@ -3,6 +3,4 @@ from django.http import HttpResponse
 from django.template import loader
 
 def home_show(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
 
