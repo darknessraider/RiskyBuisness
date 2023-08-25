dc08590ce62bd4045acd4bae167abe550ad382e8
Gray486 <me@grayjn.com>
2023-08-25 11:06:03 -0400

    Add login system

--- RiskyBuisness/login/views.py
+++ RiskyBuisness/login/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from .forms import LoginForm
 from django.http import HttpResponseRedirect
+from .models import Member
 
 def login_show(request):
+
     if request.method == "POST":
         
         form = LoginForm(request.POST)
         if form.is_valid():
             form.cleaned_data
-            request.session["username"] = form.cleaned_data['username']
-            request.session["password"] = form.cleaned_data['password']
-            return HttpResponseRedirect('/')
+            
+            user = Member.objects.filter(username=form.cleaned_data['username']).values()
+
+            if user and (user[0]['password'] == form.cleaned_data['password']):
+                request.session["username"] = form.cleaned_data['username']
+                request.session["password"] = form.cleaned_data['password']
+                return HttpResponseRedirect('/')
+            
+            else:
+                return render(request, "login.html", {"msg": 'Incorrect username or password.', "form": form})
+      
     else:
         form = LoginForm()
 
