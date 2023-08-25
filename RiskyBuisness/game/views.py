27109231674d6f34bda754badf0f477ac1b4df35
Gray486 <me@grayjn.com>
2023-08-25 17:39:20 -0400

    Add profile manager to navbar

--- RiskyBuisness/game/views.py
+++ RiskyBuisness/game/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from django.template import loader
 
 def game_show(request):
+    
+    if 'username' in request.session :
+        username = request.session["username"]
+    else:
+        username = None
+
     template = loader.get_template('game.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render({'username': username}))
 
