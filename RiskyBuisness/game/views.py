06e5366000d85f0a0597662093a85c7328217d16
Gray486 <me@grayjn.com>
2023-08-29 12:28:59 -0400

    Add post request to game page

--- RiskyBuisness/game/views.py
+++ RiskyBuisness/game/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import HttpResponseBadRequest, JsonResponse
+import json
 
 def game_show(request):
+
+    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    if is_ajax:
+        if request.method == 'POST':
+            data = json.load(request)
+            print(data["payload"])
+            return JsonResponse({'status': 'Valid request'})
+        return JsonResponse({'status': 'Invalid request'}, status=400)
     
     if 'username' in request.session :
         username = request.session["username"]
---
+++ RiskyBuisness/static/game/js/main.js
@@ -0,0 +1,33 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + "=")) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+}
+
+function postData(string) {
+
+    fetch("/game/", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+          "X-CSRFToken": getCookie("csrftoken"),
+        },
+        body: JSON.stringify({payload: string})
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+    });
+
+}
